c051bd5b756c5a65534b55cc80271bc2d7fd6eae
ROMANVIKI <vikiadhi2016@gmail.com>
2023-12-26 12:35:41 +0530

    final commit

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from .models import Order
 
+#for indian zip code verification
+from localflavor.in_.forms import INZipCodeField 
+
 
 class OrderCreateForm(forms.ModelForm):
+    postal_code = INZipCodeField()
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.urls import reverse
-from parler.models import TranslatableModel, TranslatedFields
+
 # Create your models here.
 
 
-class Category(TranslatableModel):
-
-    translations = TranslatedFields(
-        name = models.CharField(max_length=200),
-        slug = models.SlugField(max_length=200, unique=True),)
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ['name']
@@ -27,17 +25,12 @@ class Category(TranslatableModel):
         return reverse('shop:product_list_by_category', args=[self.slug])
     
 
-class Product(TranslatableModel):
-    translations = TranslatedFields(
-        name = models.CharField(max_length=200),
-        slug = models.SlugField(max_length=200),
-        description = models.TextField(blank=True)
-    )
-
+class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-   
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
